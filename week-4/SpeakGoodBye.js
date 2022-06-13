95050ee995a96fd7ad36978ee97e3053c635ca7d
Gabbydamian <96208680+Gabbydamian@users.noreply.github.com>
2022-06-13 10:19:22 +0100

    update SpeakGoodBye.js

--- week-4/SpeakGoodBye.js
+++ week-4/SpeakGoodBye.js
@@ -12,7 +12,15 @@
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 
+(function(window) {
 
+	var speakWord = "Good Bye";
+
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	window.byeSpeaker = byeSpeaker;
+})(window);
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
@@ -23,12 +31,4 @@
 // xxxx.xxxx = byeSpeaker;
 
 
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
 
-})(window);
